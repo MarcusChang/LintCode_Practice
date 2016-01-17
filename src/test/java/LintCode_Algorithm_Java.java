85ea3c2161daa5382c3dc09a215b0a357d643876
Marcus Chang <marcus_chang@aliyun.com>
2016-01-18 00:42:36 +0800

    LintCode Algorithm Practice
    Updated:
    http://www.lintcode.com/zh-cn/problem/unique-paths-ii/
    
    Marcus_Chang
    
    2016-JAN-18

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -891,6 +891,51 @@ public class LintCode_Algorithm_Java {
     }
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/unique-paths-ii/
+     * @param obstacleGrid: A list of lists of integers.
+     * @return: An integer
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        // write your code here
+        if (obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0].length == 0) {
+            return 0;
+        }
+
+        int n = obstacleGrid.length;
+        int m = obstacleGrid[0].length;
+        int[][] paths = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            if (obstacleGrid[i][0] != 1) {
+                paths[i][0] = 1;
+            } else {
+                break;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            if (obstacleGrid[0][i] != 1) {
+                paths[0][i] = 1;
+            } else {
+                break;
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (obstacleGrid[i][j] != 1) {
+                    paths[i][j] = paths[i - 1][j] + paths[i][j - 1];
+                } else {
+                    paths[i][j] = 0;
+                }
+            }
+        }
+
+        return paths[n - 1][m - 1];
+    }
+
+
 
 
 
