00b8096fe77c7e40f2b710b54e8a54ddef995ce4
Marcus Chang <marcus_chang@aliyun.com>
2016-01-21 00:59:54 +0800

    LintCode Algorithm Practice
    Chapter 6 Begins
    Added ListNode.class
    
    Marcus_Chang
    
    2016-JAN-21

---
+++ src/test/java/ListNode.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Marcus_Chang on 2016/1/21.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
