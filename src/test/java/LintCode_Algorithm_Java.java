bd23bbde2874af61190f6cfa7579e758936003d5
Marcus Chang <marcus_chang@aliyun.com>
2016-01-23 02:14:26 +0800

    LintCode Algorithm Practice
    Update Chapter 8
    finished push
    
    Chapter 9 Begins
    
    Marcus_Chang
    
    2016-JAN-23

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -2049,6 +2049,40 @@ public class LintCode_Algorithm_Java {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/largest-rectangle-in-histogram/
+     * @param height: A list of integer
+     * @return: The area of largest rectangle in the histogram
+     */
+    public int largestRectangleArea(int[] height) {
+        // write your code here
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+
+        Stack<Integer> stack = new Stack<Integer>();
+        int max = 0;
+        for (int i = 0; i <= height.length; i++) {
+            int curt = (i == height.length) ? -1 : height[i];
+            while (!stack.isEmpty() && curt <= height[stack.peek()]) {
+                int h = height[stack.pop()];
+                int w = stack.isEmpty() ? i : i - stack.peek() - 1;
+                max = Math.max(max, h * w);
+            }
+            stack.push(i);
+        }
+
+        return max;
+    }
+
+
+
+    /*
+    * JiuZhang Chapter 9 begins !
+    * */
+
+
+
 
 
 
