b9b9313a1fd629b9d9ff9c7c3a25671dbefec163
Marcus Chang <marcus_chang@aliyun.com>
2016-01-23 02:21:19 +0800

    LintCode Algorithm Practice
    Chapter 9 finish push
    
    Marcus_Chang
    
    2016-JAN-23

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -2082,28 +2082,191 @@ public class LintCode_Algorithm_Java {
     * */
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/palindrome-partitioning/
+     * @param s: A string
+     * @return: A list of lists of string
+     */
+    public ArrayList<ArrayList<String>> partition(String s) {
+        ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+        if (s == null) {
+            return result;
+        }
+
+        ArrayList<String> path = new ArrayList<String>();
+        helper(s, path, 0, result);
+
+        return result;
+    }
+
+    private boolean isPalindrome(String s) {
+        int beg = 0;
+        int end = s.length() - 1;
+        while (beg < end) {
+            if (s.charAt(beg) != s.charAt(end)) {
+                return false;
+            }
+
+            beg++;
+            end--;
+        }
+
+        return true;
+    }
+
+    private void helper(String s, ArrayList<String> path, int pos,
+                        ArrayList<ArrayList<String>> result) {
+        if (pos == s.length()) {
+            result.add(new ArrayList<String>(path));
+            return;
+        }
+
+        for (int i = pos + 1; i <= s.length(); i++) {
+            String prefix = s.substring(pos, i);
+            if (!isPalindrome(prefix)) {
+                continue;
+            }
+
+            path.add(prefix);
+            helper(s, path, i, result);
+            path.remove(path.size() - 1);
+        }
+    }
 
 
 
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/combination-sum/
+     * @param candidates: A list of integers
+     * @param target:An integer
+     * @return: A list of lists of integers
+     */
+    public  ArrayList<ArrayList<Integer>> combinationSum(int[] candidates, int target) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        if (candidates == null) {
+            return result;
+        }
+
+        ArrayList<Integer> path = new ArrayList<Integer>();
+        Arrays.sort(candidates);
+        helper(candidates, target, path, 0, result);
+
+        return result;
+    }
+
+    void helper(int[] candidates, int target, ArrayList<Integer> path, int index,
+                ArrayList<ArrayList<Integer>> result) {
+        if (target == 0) {
+            result.add(new ArrayList<Integer>(path));
+            return;
+        }
+
+        int prev = -1;
+        for (int i = index; i < candidates.length; i++) {
+            if (candidates[i] > target) {
+                break;
+            }
+
+            if (prev != -1 && prev == candidates[i]) {
+                continue;
+            }
+
+            path.add(candidates[i]);
+            helper(candidates, target - candidates[i], path, i, result);
+            path.remove(path.size() - 1);
+
+            prev = candidates[i];
+        }
+    }
 
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/word-ladder-ii/
+     * @param start, a string
+     * @param end, a string
+     * @param dict, a set of string
+     * @return a list of lists of string
+     */
+    public List<List<String>> findLadders(String start, String end,
+                                          Set<String> dict) {
+        List<List<String>> ladders = new ArrayList<List<String>>();
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        Map<String, Integer> distance = new HashMap<String, Integer>();
 
+        dict.add(start);
+        dict.add(end);
 
+        bfs(map, distance, start, end, dict);
 
+        List<String> path = new ArrayList<String>();
 
+        dfs(ladders, path, end, start, distance, map);
 
+        return ladders;
+    }
 
+    void dfs(List<List<String>> ladders, List<String> path, String crt,
+             String start, Map<String, Integer> distance,
+             Map<String, List<String>> map) {
+        path.add(crt);
+        if (crt.equals(start)) {
+            Collections.reverse(path);
+            ladders.add(new ArrayList<String>(path));
+            Collections.reverse(path);
+        } else {
+            for (String next : map.get(crt)) {
+                if (distance.containsKey(next) && distance.get(crt) == distance.get(next) + 1) {
+                    dfs(ladders, path, next, start, distance, map);
+                }
+            }
+        }
+        path.remove(path.size() - 1);
+    }
 
+    void bfs(Map<String, List<String>> map, Map<String, Integer> distance,
+             String start, String end, Set<String> dict) {
+        Queue<String> q = new LinkedList<String>();
+        q.offer(start);
+        distance.put(start, 0);
+        for (String s : dict) {
+            map.put(s, new ArrayList<String>());
+        }
 
+        while (!q.isEmpty()) {
+            String crt = q.poll();
 
+            List<String> nextList = expand(crt, dict);
+            for (String next : nextList) {
+                map.get(next).add(crt);
+                if (!distance.containsKey(next)) {
+                    distance.put(next, distance.get(crt) + 1);
+                    q.offer(next);
+                }
+            }
+        }
+    }
 
+    List<String> expand(String crt, Set<String> dict) {
+        List<String> expansion = new ArrayList<String>();
 
+        for (int i = 0; i < crt.length(); i++) {
+            for (char ch = 'a'; ch <= 'z'; ch++) {
+                if (ch != crt.charAt(i)) {
+                    String expanded = crt.substring(0, i) + ch
+                            + crt.substring(i + 1);
+                    if (dict.contains(expanded)) {
+                        expansion.add(expanded);
+                    }
+                }
+            }
+        }
 
+        return expansion;
+    }
 
 }
-
