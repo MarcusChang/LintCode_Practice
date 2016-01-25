5c06fbab8ae557200f83df95a98f33d105025740
Marcus Chang <marcus_chang@aliyun.com>
2016-01-26 00:08:06 +0800

    LintCode Algorithm Practice
    Chapter 9 finished push
    
    Marcus_Chang
    
    2016-JAN-26

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -2309,4 +2309,49 @@ public class LintCode_Algorithm_Java {
         return result;
     }
 
+
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/clone-graph/
+     * @param node: A undirected graph node
+     * @return: A undirected graph node
+     */
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        // write your code here
+        if (node == null) {
+            return null;
+        }
+
+        ArrayList<UndirectedGraphNode> nodes = new ArrayList<UndirectedGraphNode>();
+        HashMap<UndirectedGraphNode, UndirectedGraphNode> map
+                = new HashMap<UndirectedGraphNode, UndirectedGraphNode>();
+
+        // clone nodes
+        nodes.add(node);
+        map.put(node, new UndirectedGraphNode(node.label));
+
+        int start = 0;
+        while (start < nodes.size()) {
+            UndirectedGraphNode head = nodes.get(start++);
+            for (int i = 0; i < head.neighbors.size(); i++) {
+                UndirectedGraphNode neighbor = head.neighbors.get(i);
+                if (!map.containsKey(neighbor)) {
+                    map.put(neighbor, new UndirectedGraphNode(neighbor.label));
+                    nodes.add(neighbor);
+                }
+            }
+        }
+
+        // clone neighbors
+        for (int i = 0; i < nodes.size(); i++) {
+            UndirectedGraphNode newNode = map.get(nodes.get(i));
+            for (int j = 0; j < nodes.get(i).neighbors.size(); j++) {
+                newNode.neighbors.add(map.get(nodes.get(i).neighbors.get(j)));
+            }
+        }
+
+        return map.get(node);
+
+    }
+
 }
