5e96d2edc27cdd9c587b50c2b20392974b6760fc
Marcus Chang <marcus_chang@aliyun.com>
2016-01-25 23:42:33 +0800

    LintCode Algorithm Practice
    Chapter 9 update push
    
    Marcus_Chang
    
    2016-JAN-25

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -2269,4 +2269,44 @@ public class LintCode_Algorithm_Java {
         return expansion;
     }
 
+
+
+    /**
+     * http://www.jiuzhang.com/solutions/topological-sorting/
+     * @param graph: A list of Directed graph node
+     * @return: Any topological order for the given graph.
+     */
+    public ArrayList<DirectedGraphNode> topSort(ArrayList<DirectedGraphNode> graph) {
+        // write your code here
+        ArrayList<DirectedGraphNode> result = new ArrayList<DirectedGraphNode>();
+        HashMap<DirectedGraphNode, Integer> map = new HashMap();
+        for (DirectedGraphNode node : graph) {
+            for (DirectedGraphNode neighbor : node.neighbors) {
+                if (map.containsKey(neighbor)) {
+                    map.put(neighbor, map.get(neighbor) + 1);
+                } else {
+                    map.put(neighbor, 1);
+                }
+            }
+        }
+        Queue<DirectedGraphNode> q = new LinkedList<DirectedGraphNode>();
+        for (DirectedGraphNode node : graph) {
+            if (!map.containsKey(node)) {
+                q.offer(node);
+                result.add(node);
+            }
+        }
+        while (!q.isEmpty()) {
+            DirectedGraphNode node = q.poll();
+            for (DirectedGraphNode n : node.neighbors) {
+                map.put(n, map.get(n) - 1);
+                if (map.get(n) == 0) {
+                    result.add(n);
+                    q.offer(n);
+                }
+            }
+        }
+        return result;
+    }
+
 }
