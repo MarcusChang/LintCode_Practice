6b859f612cbb316384d0e3b3d5ee994d13eec4c4
Marcus Chang <marcus_chang@aliyun.com>
2016-01-28 01:13:33 +0800

    LintCode Algorithm Practice
    Chapter 2 updated
    
    Marcus_Chang
    
    2016-JAN-28

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -617,6 +617,55 @@ public class LintCode_Algorithm_Java {
     }
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/wood-cut/
+     *@param L: Given n pieces of wood with length L[i]
+     *@param k: An integer
+     *return: The maximum length of the small pieces.
+     */
+    public int woodCut(int[] L, int k) {
+        int max = 0;
+        for (int i = 0; i < L.length; i++) {
+            max = Math.max(max, L[i]);
+        }
+
+        // find the largest length that can cut more than k pieces of wood.
+        int start = 1, end = max;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (count(L, mid) >= k) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (count(L, end) >= k) {
+            return end;
+        }
+        if (count(L, start) >= k) {
+            return start;
+        }
+        return 0;
+    }
+
+    private int count(int[] L, int length) {
+        int sum = 0;
+        for (int i = 0; i < L.length; i++) {
+            sum += L[i] / length;
+        }
+        return sum;
+    }
+
+
+
+
+
+
+
+
+
+
 
 
     /*
