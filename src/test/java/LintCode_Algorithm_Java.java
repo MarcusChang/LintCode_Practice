92ad46e6010d25f20048457396560550b3a16993
Marcus Chang <marcus_chang@aliyun.com>
2016-01-29 02:00:35 +0800

    LintCode Algorithm Practice
    Chapter 2 updated search2DMatrix2
    
    Marcus_Chang
    
    2016-JAN-29

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -692,6 +692,44 @@ public class LintCode_Algorithm_Java {
     }
 
 
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/search-a-2d-matrix-ii/
+     * @param matrix: A list of lists of integers
+     * @param: A number you want to search in the matrix
+     * @return: An integer indicate the occurrence of target in the given matrix
+     */
+    public int search2DMatrix2(int[][] matrix, int target) {
+        // write your code here
+        if (matrix == null || matrix.length == 0) {
+            return 0;
+        }
+        if (matrix[0] == null || matrix[0].length == 0) {
+            return 0;
+        }
+
+        // from bottom left to top right
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int x = n - 1;
+        int y = 0;
+        int count = 0;
+
+        while (x >= 0 && y < m) {
+            if (matrix[x][y] < target) {
+                y++;
+            } else if (matrix[x][y] > target) {
+                x--;
+            } else {
+                count++;
+                x--;
+                y++;
+            }
+        }
+        return count;
+    }
+
+
     /*
     * JiuZhang Chapter 3 begins !
     * */
