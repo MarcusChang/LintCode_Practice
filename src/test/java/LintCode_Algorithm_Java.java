0b6e469005f806357f620fd556feba5b31f24397
Marcus Chang <marcus_chang@aliyun.com>
2016-01-31 03:05:46 +0800

    LintCode Algorithm Practice
    Chapter 3 updated http://www.lintcode.com/zh-cn/problem/symmetric-binary-tree/
    
    Marcus_Chang
    
    2016-JAN-31

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -1122,6 +1122,36 @@ public class LintCode_Algorithm_Java {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/symmetric-binary-tree/
+     * @param root, the root of binary tree.
+     * @return true if it is a mirror of itself, or false.
+     */
+    public boolean isSymmetric(TreeNode root) {
+        // Write your code here
+        // Write your code here
+        if (root == null) {
+            return true;
+        }
+        return check(root.left, root.right);
+    }
+
+    private boolean check(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+        if (root1 == null || root2 == null) {
+            return false;
+        }
+        if (root1.val != root2.val) {
+            return false;
+        }
+        return check(root1.left, root2.right) && check(root1.right, root2.left);
+    }
+
+
+
+
 
      /*
     * JiuZhang Chapter 4 begins !
