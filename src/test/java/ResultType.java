3afd8f826db94e32c9b1fe7354e5204c7bd9a030
Marcus Chang <marcus_chang@aliyun.com>
2016-01-31 03:16:18 +0800

    LintCode Algorithm Practice
    ResultType structure added
    
    Marcus_Chang
    
    2016-JAN-31

---
+++ src/test/java/ResultType.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Marcus_Chang on 2016/1/31.
+ */
+public class ResultType {
+    public int depth;
+    public boolean isFull, isComplete;
+    ResultType(int depth, boolean isFull, boolean isComplete) {
+        this.depth = depth;
+        this.isFull = isFull;
+        this.isComplete = isComplete;
+    }
+}
