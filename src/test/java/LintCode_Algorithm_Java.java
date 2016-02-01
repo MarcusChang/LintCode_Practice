97daa454f924dd642150ef43aa08703af73ee919
Marcus Chang <marcus_chang@aliyun.com>
2016-02-02 01:00:26 +0800

    LintCode Algorithm Practice
    Chapter 3 updated http://www.lintcode.com/zh-cn/problem/construct-binary-tree-from-preorder-and-inorder-traversal/
    
    Marcus_Chang
    
    2016-FEB-02

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -1227,6 +1227,48 @@ public class LintCode_Algorithm_Java {
     }
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/construct-binary-tree-from-preorder-and-inorder-traversal/
+     *@param preorder : A list of integers that preorder traversal of a tree
+     *@param inorder : A list of integers that inorder traversal of a tree
+     *@return : Root of a tree
+     */
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (inorder.length != preorder.length) {
+            return null;
+        }
+        return myBuildTree(inorder, 0, inorder.length - 1, preorder, 0, preorder.length - 1);
+    }
+
+    private int findPosition(int[] arr, int start, int end, int key) {
+        int i;
+        for (i = start; i <= end; i++) {
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private TreeNode myBuildTree(int[] inorder, int instart, int inend,
+                                 int[] preorder, int prestart, int preend) {
+        if (instart > inend) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(preorder[prestart]);
+        int position = findPosition(inorder, instart, inend, preorder[prestart]);
+
+        root.left = myBuildTree(inorder, instart, position - 1,
+                preorder, prestart + 1, prestart + position - instart);
+        root.right = myBuildTree(inorder, position + 1, inend,
+                preorder, position - inend + preend + 1, preend);
+        return root;
+    }
+
+
+
+
 
 
 
