72746ca110cab08657a0fb5e76ba649541783cac
Marcus Chang <marcus_chang@aliyun.com>
2016-02-04 02:03:39 +0800

    LintCode Algorithm Practice
    Chapter 3 updated http://www.lintcode.com/zh-cn/problem/binary-tree-serialization/ (2)
    
    Marcus_Chang
    
    2016-FEB-04

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -1449,6 +1449,7 @@ public class LintCode_Algorithm_Java {
 
 
     /**
+     * http://www.lintcode.com/zh-cn/problem/binary-tree-serialization/
      * This method will be invoked first, you should design your own algorithm
      * to serialize a binary tree which denote by a root node to a string which
      * can be easily deserialized by your own "deserialize" method later.
@@ -1490,6 +1491,44 @@ public class LintCode_Algorithm_Java {
         return sb.toString();
     }
 
+
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/binary-tree-serialization/
+     * This method will be invoked second, the argument data is what exactly
+     * you serialized at method "serialize", that means the data is not given by
+     * system, it's given by your own serialize method. So the format of data is
+     * designed by yourself, and deserialize it here as you serialize it in
+     * "serialize" method.
+     */
+    public TreeNode deserialize(String data) {
+        // write your code here
+        if (data.equals("{}")) {
+            return null;
+        }
+        String[] vals = data.substring(1, data.length() - 1).split(",");
+        ArrayList<TreeNode> queue = new ArrayList<TreeNode>();
+        TreeNode root = new TreeNode(Integer.parseInt(vals[0]));
+        queue.add(root);
+        int index = 0;
+        boolean isLeftChild = true;
+        for (int i = 1; i < vals.length; i++) {
+            if (!vals[i].equals("#")) {
+                TreeNode node = new TreeNode(Integer.parseInt(vals[i]));
+                if (isLeftChild) {
+                    queue.get(index).left = node;
+                } else {
+                    queue.get(index).right = node;
+                }
+                queue.add(node);
+            }
+            if (!isLeftChild) {
+                index++;
+            }
+            isLeftChild = !isLeftChild;
+        }
+        return root;
+    }
 
 
 
