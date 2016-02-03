2cfbafeb6c202ac3fa0c9c1667be42c12ba1afb4
Marcus Chang <marcus_chang@aliyun.com>
2016-02-04 02:02:31 +0800

    LintCode Algorithm Practice
    Chapter 3 updated http://www.lintcode.com/zh-cn/problem/binary-tree-serialization/ (1)
    
    Marcus_Chang
    
    2016-FEB-04

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -1448,6 +1448,48 @@ public class LintCode_Algorithm_Java {
 
 
 
+    /**
+     * This method will be invoked first, you should design your own algorithm
+     * to serialize a binary tree which denote by a root node to a string which
+     * can be easily deserialized by your own "deserialize" method later.
+     */
+    public String serialize(TreeNode root) {
+        // write your code here
+        if (root == null) {
+            return "{}";
+        }
+
+        ArrayList<TreeNode> queue = new ArrayList<TreeNode>();
+        queue.add(root);
+
+        for (int i = 0; i < queue.size(); i++) {
+            TreeNode node = queue.get(i);
+            if (node == null) {
+                continue;
+            }
+            queue.add(node.left);
+            queue.add(node.right);
+        }
+
+        while (queue.get(queue.size() - 1) == null) {
+            queue.remove(queue.size() - 1);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        sb.append(queue.get(0).val);
+        for (int i = 1; i < queue.size(); i++) {
+            if (queue.get(i) == null) {
+                sb.append(",#");
+            } else {
+                sb.append(",");
+                sb.append(queue.get(i).val);
+            }
+        }
+        sb.append("}");
+        return sb.toString();
+    }
+
 
 
 
