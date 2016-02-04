5aae6b9f1b94a4ee871ada5342af3cbdebfc9acb
Marcus Chang <marcus_chang@aliyun.com>
2016-02-05 01:53:29 +0800

    LintCode Algorithm Practice
    Chapter 3 updated http://www.lintcode.com/zh-cn/problem/remove-node-in-binary-search-tree/
    
    Marcus_Chang
    
    2016-FEB-05

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -1533,6 +1533,82 @@ public class LintCode_Algorithm_Java {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/remove-node-in-binary-search-tree/
+     * @param root: The root of the binary search tree.
+     * @param value: Remove the node with given value.
+     * @return: The root of the binary search tree after removal.
+     */
+    public TreeNode removeNode(TreeNode root, int value) {
+        // write your code here
+        TreeNode dummy = new TreeNode(0);
+        dummy.left = root;
+
+        TreeNode parent = findNode(dummy, root, value);
+        TreeNode node;
+        if (parent.left != null && parent.left.val == value) {
+            node = parent.left;
+        } else if (parent.right != null && parent.right.val == value) {
+            node = parent.right;
+        } else {
+            return dummy.left;
+        }
+
+        deleteNode(parent, node);
+
+        return dummy.left;
+    }
+
+    private TreeNode findNode(TreeNode parent, TreeNode node, int value) {
+        if (node == null) {
+            return parent;
+        }
+
+        if (node.val == value) {
+            return parent;
+        }
+        if (value < node.val) {
+            return findNode(node, node.left, value);
+        } else {
+            return findNode(node, node.right, value);
+        }
+    }
+
+    private void deleteNode(TreeNode parent, TreeNode node) {
+        if (node.right == null) {
+            if (parent.left == node) {
+                parent.left = node.left;
+            } else {
+                parent.right = node.left;
+            }
+        } else {
+            TreeNode temp = node.right;
+            TreeNode father = node;
+
+            while (temp.left != null) {
+                father = temp;
+                temp = temp.left;
+            }
+
+            if (father.left == temp) {
+                father.left = temp.right;
+            } else {
+                father.right = temp.right;
+            }
+
+            if (parent.left == node) {
+                parent.left = temp;
+            } else {
+                parent.right = temp;
+            }
+
+            temp.left = node.left;
+            temp.right = node.right;
+        }
+    }
+
+
+
 
      /*
     * JiuZhang Chapter 4 begins !
