01a19b5ab1e8b523bb5fc2b1626a4db9a89723aa
Marcus Chang <marcus_chang@aliyun.com>
2016-02-06 00:18:13 +0800

    LintCode Algorithm Practice
    Chapter 5 updated http://www.lintcode.com/zh-cn/problem/wildcard-matching/
    
    Marcus_Chang
    
    2016-FEB-06

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -2061,6 +2061,54 @@ public class LintCode_Algorithm_Java {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/wildcard-matching/
+     * @param s: A string
+     * @param p: A string includes "?" and "*"
+     * @return: A boolean
+     */
+    public boolean isMatch(String s, String p) {
+        // write your code here
+        // without this optimization, it will fail for large data set
+        int plenNoStar = 0;
+        for (char c : p.toCharArray())
+            if (c != '*') plenNoStar++;
+        if (plenNoStar > s.length()) return false;
+
+        s = " " + s;
+        p = " " + p;
+        int slen = s.length();
+        int plen = p.length();
+
+        boolean[] dp = new boolean[slen];
+        TreeSet<Integer> firstTrueSet = new TreeSet<Integer>();
+        firstTrueSet.add(0);
+        dp[0] = true;
+
+        boolean allStar = true;
+        for (int pi = 1; pi < plen; pi++) {
+            if (p.charAt(pi) != '*')
+                allStar = false;
+            for (int si = slen - 1; si >= 0; si--) {
+                if (si == 0) {
+                    dp[si] = allStar ? true : false;
+                } else if (p.charAt(pi) != '*') {
+                    if (s.charAt(si) == p.charAt(pi) || p.charAt(pi) == '?') dp[si] = dp[si-1];
+                    else dp[si] = false;
+                } else {
+                    int firstTruePos = firstTrueSet.isEmpty() ? Integer.MAX_VALUE : firstTrueSet.first();
+                    if (si >= firstTruePos) dp[si] = true;
+                    else dp[si] = false;
+                }
+                if (dp[si]) firstTrueSet.add(si);
+                else firstTrueSet.remove(si);
+            }
+        }
+        return dp[slen - 1];
+    }
+
+
+
 
      /*
     * JiuZhang Chapter 6 begins !
