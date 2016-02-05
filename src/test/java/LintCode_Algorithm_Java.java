6ef64b9bfbc2ea8185fd2e7fe5074e1ad63db92c
Marcus Chang <marcus_chang@aliyun.com>
2016-02-05 23:56:24 +0800

    LintCode Algorithm Practice
    Chapter 5 updated http://www.lintcode.com/zh-cn/problem/backpack/
    
    Marcus_Chang
    
    2016-FEB-05

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -2024,6 +2024,44 @@ public class LintCode_Algorithm_Java {
 
 
 
+
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/backpack/
+     * @param m: An integer m denotes the size of a backpack
+     * @param A: Given n items with size A[i]
+     * @return: The maximum size
+     */
+    public int backPack(int m, int[] A) {
+        // write your code here
+        boolean f[][] = new boolean[A.length + 1][m + 1];
+        for (int i = 0; i <= A.length; i++) {
+            for (int j = 0; j <= m; j++) {
+                f[i][j] = false;
+            }
+        }
+        f[0][0] = true;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j <= m; j++) {
+                f[i + 1][j] = f[i][j];
+                if (j >= A[i] && f[i][j - A[i]]) {
+                    f[i + 1][j] = true;
+                }
+            } // for j
+        } // for i
+
+        for (int i = m; i >= 0; i--) {
+            if (f[A.length][i]) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+
+
+
+
      /*
     * JiuZhang Chapter 6 begins !
     * */
