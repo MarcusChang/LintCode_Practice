01dd90f2ad932aeee98e02673d26e77f87d11e5e
Marcus Chang <marcus_chang@aliyun.com>
2016-02-08 01:06:44 +0800

    LintCode Algorithm Practice
    Chapter 6 updated http://www.lintcode.com/zh-cn/problem/remove-linked-list-elements/
    
    Marcus_Chang
    
    2016-FEB-08

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -2402,6 +2402,32 @@ public class LintCode_Algorithm_Java {
 
 
 
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/remove-linked-list-elements/
+     * @param head a ListNode
+     * @param val an integer
+     * @return a ListNode
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        // Write your code here
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        head = dummy;
+
+        while (head.next != null) {
+            if (head.next.val == val) {
+                head.next = head.next.next;
+            } else {
+                head = head.next;
+            }
+        }
+
+        return dummy.next;
+    }
+
+
+
      /*
     * JiuZhang Chapter 7 begins !
     * */
