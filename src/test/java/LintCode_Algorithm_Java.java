22d4416a7664fbf90e7df05a01ab40b24e86315a
Marcus Chang <marcus_chang@aliyun.com>
2016-02-15 00:19:07 +0800

    LintCode Algorithm Practice
    Chapter 6 updated http://www.lintcode.com/zh-cn/problem/add-two-numbers-ii/
    
    Marcus_Chang
    
    2016-FEB-15

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -2587,6 +2587,75 @@ public class LintCode_Algorithm_Java {
 
 
 
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/add-two-numbers-ii/
+     * @param l1: the first list
+     * @param l2: the second list
+     * @return: the sum list of l1 and l2
+     */
+    public ListNode addLists2(ListNode l1, ListNode l2) {
+        // write your code here
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+
+        return reverse(addList1(l1, l2));
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+
+    private ListNode addList1(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        int carry = 0;
+
+        while (l1 != null && l2 != null) {
+            tail.next = new ListNode((l1.val + l2.val + carry) % 10);
+            tail = tail.next;
+            carry = (l1.val + l2.val + carry) / 10;
+
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        while (l1 != null) {
+            tail.next = new ListNode((l1.val + carry) % 10);
+            tail = tail.next;
+            carry = (l1.val + carry) / 10;
+
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            tail.next = new ListNode((l2.val + carry) % 10);
+            tail = tail.next;
+            carry = (l2.val + carry) / 10;
+
+            l2 = l2.next;
+        }
+
+        while (carry != 0) {
+            tail.next = new ListNode(carry % 10);
+            tail = tail.next;
+            carry = carry / 10;
+        }
+
+        return dummy.next;
+    }
+
+
+
+
+
+
      /*
     * JiuZhang Chapter 7 begins !
     * */
