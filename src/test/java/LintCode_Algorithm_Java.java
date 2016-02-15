9aa24c6656a74494ef03f224664cf48130824c9e
Marcus Chang <marcus_chang@aliyun.com>
2016-02-16 00:54:27 +0800

    LintCode Algorithm Practice
    Chapter 6 updated http://www.lintcode.com/zh-cn/problem/palindrome-linked-list/
    
    Marcus_Chang
    
    2016-FEB-16

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -2654,6 +2654,58 @@ public class LintCode_Algorithm_Java {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/palindrome-linked-list/
+     * @param head a ListNode
+     * @return a boolean
+     */
+    public boolean isPalindromeLinkedList(ListNode head) {
+        // Write your code here
+        if (head == null) {
+            return true;
+        }
+
+        ListNode middle = findMiddle(head);
+        middle.next = reversePalindromeLinkedList(middle.next);
+
+        ListNode p1 = head, p2 = middle.next;
+        while (p1 != null && p2 != null && p1.val == p2.val) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        return p2 == null;
+    }
+
+    private ListNode findMiddle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    private ListNode reversePalindromeLinkedList(ListNode head) {
+        ListNode prev = null;
+
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+
+        return prev;
+    }
+
+
+
+
 
 
      /*
