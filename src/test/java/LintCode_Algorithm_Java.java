6195d5f5042c0026f8a7cf73318dfe352a256f84
Marcus Chang <marcus_chang@aliyun.com>
2016-02-19 01:34:39 +0800

    LintCode Algorithm Practice
    Chapter 6 updated http://www.lintcode.com/zh-cn/problem/reverse-nodes-in-k-group/
    
    Marcus_Chang
    
    2016-FEB-19

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -2778,6 +2778,60 @@ public class LintCode_Algorithm_Java {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/reverse-nodes-in-k-group/
+     * @param head a ListNode
+     * @param k an integer
+     * @return a ListNode
+     */
+    public ListNode reverseKGroup(ListNode head, int k) {
+        // Write your code here
+        if (head == null || k <= 1) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        head = dummy;
+        while (head.next != null) {
+            head = reverseNextK(head, k);
+        }
+
+        return dummy.next;
+    }
+
+    // reverse head->n1->..->nk->next..
+    // to head->nk->..->n1->next..
+    // return n1
+    private ListNode reverseNextK(ListNode head, int k) {
+        // check there is enought nodes to reverse
+        ListNode next = head; // next is not null
+        for (int i = 0; i < k; i++) {
+            if (next.next == null) {
+                return next;
+            }
+            next = next.next;
+        }
+
+        // reverse
+        ListNode n1 = head.next;
+        ListNode prev = head, curt = n1;
+        for (int i = 0; i < k; i++) {
+            ListNode temp = curt.next;
+            curt.next = prev;
+            prev = curt;
+            curt = temp;
+        }
+
+        n1.next = curt;
+        head.next = prev;
+        return n1;
+    }
+
+
+
+
 
      /*
     * JiuZhang Chapter 7 begins !
