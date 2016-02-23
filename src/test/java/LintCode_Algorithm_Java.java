4cab60a0fec3d1f5a1012a41e77452b5711ea851
Marcus Chang <marcus_chang@aliyun.com>
2016-02-24 01:21:07 +0800

    LintCode Algorithm Practice
    Chapter 7 updated http://www.lintcode.com/zh-cn/problem/minimum-subarray/
    
    Marcus_Chang
    
    2016-FEB-23

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -3275,6 +3275,34 @@ public class LintCode_Algorithm_Java {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/minimum-subarray/
+     * @param nums: a list of integers
+     * @return: A integer indicate the sum of minimum subarray
+     */
+    public int minSubArray(ArrayList<Integer> nums) {
+        // write your code
+        if (nums == null)
+            return 0;
+        int len = nums.size();
+        int min = Integer.MAX_VALUE, currSum = 0;
+        int[] localmin  = new int[len];
+        int[] globalmin = new int[len];
+        for (int i = 0; i < len; i++) {
+            if( i ==0 )
+                globalmin[i] = localmin[i] = nums.get(i);
+            else {
+                localmin[i] = Math.min(localmin[i - 1] + nums.get(i), nums.get(i));
+                globalmin[i] = Math.min(globalmin[i - 1], localmin[i]);
+            }
+
+        }
+        return globalmin[len-1];
+    }
+
+
+
+
 
 
      /*
