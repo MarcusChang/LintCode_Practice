d9c0d29506a6ca00356b3ddda50e76120ec7a864
Marcus Chang <marcus_chang@aliyun.com>
2016-02-26 00:44:14 +0800

    LintCode Algorithm Practice
    Chapter 7 updated http://www.lintcode.com/zh-cn/problem/maximum-product-subarray/
    
    Marcus_Chang
    
    2016-FEB-26

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -3320,6 +3320,34 @@ public class LintCode_Algorithm_Java {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/maximum-product-subarray/
+     * @param nums: an array of integers
+     * @return: an integer
+     */
+    public int maxProduct(int[] nums) {
+        // write your code here
+        int[] max = new int[nums.length];
+        int[] min = new int[nums.length];
+
+        min[0] = max[0] = nums[0];
+        int result = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            min[i] = max[i] = nums[i];
+            if (nums[i] > 0) {
+                max[i] = Math.max(max[i], max[i - 1] * nums[i]);
+                min[i] = Math.min(min[i], min[i - 1] * nums[i]);
+            } else if (nums[i] < 0) {
+                max[i] = Math.max(max[i], min[i - 1] * nums[i]);
+                min[i] = Math.min(min[i], max[i - 1] * nums[i]);
+            }
+
+            result = Math.max(result, max[i]);
+        }
+
+        return result;
+    }
+
 
 
 
