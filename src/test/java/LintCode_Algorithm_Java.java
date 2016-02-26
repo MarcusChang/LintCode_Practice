b54a5988bd638c2fee2c8323d092dbfa0e280a7a
Marcus Chang <marcus_chang@aliyun.com>
2016-02-27 01:42:44 +0800

    LintCode Algorithm Practice
    Chapter 7 updated http://www.lintcode.com/zh-cn/problem/best-time-to-buy-and-sell-stock-iii/
    
    Marcus_Chang
    
    2016-FEB-27

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -3351,6 +3351,49 @@ public class LintCode_Algorithm_Java {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/best-time-to-buy-and-sell-stock-iii/
+     * @param prices: Given an integer array
+     * @return: Maximum profit
+     */
+    public int maxProfit(int[] prices) {
+        // write your code here
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
+
+        int[] left = new int[prices.length];
+        int[] right = new int[prices.length];
+
+        // DP from left to right;
+        left[0] = 0;
+        int min = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            min = Math.min(prices[i], min);
+            left[i] = Math.max(left[i - 1], prices[i] - min);
+        }
+
+        //DP from right to left;
+        right[prices.length - 1] = 0;
+        int max = prices[prices.length - 1];
+        for (int i = prices.length - 2; i >= 0; i--) {
+            max = Math.max(prices[i], max);
+            right[i] = Math.max(right[i + 1], max - prices[i]);
+        }
+
+        int profit = 0;
+        for (int i = 0; i < prices.length; i++){
+            profit = Math.max(left[i] + right[i], profit);
+        }
+
+        return profit;
+    }
+
+
+
+
+
+
      /*
     * JiuZhang Chapter 8 begins !
     * */
