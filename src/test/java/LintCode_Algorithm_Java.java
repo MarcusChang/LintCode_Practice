6a8defd5f9430caa75ee1296d5383775d229f55f
Marcus Chang <marcus_chang@aliyun.com>
2016-03-01 00:08:53 +0800

    LintCode Algorithm Practice
    Chapter 7 updated http://www.lintcode.com/zh-cn/problem/sort-colors-ii/
    
    Marcus_Chang
    
    2016-MAR-01

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -3433,6 +3433,60 @@ public class LintCode_Algorithm_Java {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/sort-colors-ii/
+     * @param colors: A list of integer
+     * @param k: An integer
+     * @return: nothing
+     */
+    public void sortColors2(int[] colors, int k) {
+        // write your code here
+        int count = 0;
+        int start = 0;
+        int end = colors.length-1;
+        while (count < k) {
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+
+            for (int i = start; i < end; i++) {
+                min = Math.min(min, colors[i]);
+                max = Math.max(max, colors[i]);
+            }
+            int left = start;
+            int right = end;
+            int cur = left;
+            while(cur <= right) {
+                if (colors[cur] == min) {
+                    swapColors(left, cur, colors);
+                    cur++;
+                    left++;
+                } else if (colors[cur] > min && colors[cur] < max) {
+                    cur++;
+                } else {
+                    int tmp = colors[cur];
+                    swapColors(cur, right, colors);
+                    right--;
+                }
+            }
+            count += 2;
+            start = left;
+            end = right;
+        }
+    }
+
+    void swapColors(int left, int right, int[] colors) {
+        int tmp = colors[left];
+        colors[left] = colors[right];
+        colors[right] = tmp;
+    }
+
+
+
+
+
+
+
+
 
      /*
     * JiuZhang Chapter 8 begins !
