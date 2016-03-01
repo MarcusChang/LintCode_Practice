c6c498e9d0a642d498e43280a060e9978b7b8898
Marcus Chang <marcus_chang@aliyun.com>
2016-03-02 00:26:54 +0800

    LintCode Algorithm Practice
    Chapter 7 updated http://www.lintcode.com/zh-cn/problem/single-number-iii/
    
    Marcus_Chang
    
    2016-MAR-02

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -3483,6 +3483,35 @@ public class LintCode_Algorithm_Java {
 
 
 
+    /**
+     *http://www.lintcode.com/zh-cn/problem/single-number-iii/
+     * @param A : An integer array
+     * @return : Two integers
+     */
+    public List<Integer> singleNumberIII(int[] A) {
+        // write your code here
+        int xor = 0;
+        for (int i = 0; i < A.length; i++) {
+            xor ^= A[i];
+        }
+
+        int lastBit = xor - (xor & (xor - 1));
+        int group0 = 0, group1 = 0;
+        for (int i = 0; i < A.length; i++) {
+            if ((lastBit & A[i]) == 0) {
+                group0 ^= A[i];
+            } else {
+                group1 ^= A[i];
+            }
+        }
+
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        result.add(group0);
+        result.add(group1);
+        return result;
+    }
+
+
 
 
 
