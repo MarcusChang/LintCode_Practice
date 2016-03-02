788482ecf22fad271bbd674483b14d5ab9c5d42c
Marcus Chang <marcus_chang@aliyun.com>
2016-03-03 00:08:13 +0800

    LintCode Algorithm Practice
    Chapter 7 updated http://www.lintcode.com/zh-cn/problem/single-number-ii/
    
    Marcus_Chang
    
    2016-MAR-03

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -3514,6 +3514,33 @@ public class LintCode_Algorithm_Java {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/single-number-ii/
+     * @param A : An integer array
+     * @return : An integer
+     */
+    public int singleNumberII(int[] A) {
+        // write your code here
+        if (A == null || A.length == 0) {
+            return -1;
+        }
+        int result=0;
+        int[] bits=new int[32];
+        for (int i = 0; i < 32; i++) {
+            for(int j = 0; j < A.length; j++) {
+                bits[i] += A[j] >> i & 1;
+                bits[i] %= 3;
+            }
+
+            result |= (bits[i] << i);
+        }
+        return result;
+    }
+
+
+
+
+
 
 
 
