d9812bee60b1190c03d696f20aa77fe57194d3ef
Marcus Chang <marcus_chang@aliyun.com>
2016-03-04 01:03:37 +0800

    LintCode Algorithm Practice
    Chapter 7 updated http://www.lintcode.com/zh-cn/problem/4sum/
    
    Marcus_Chang
    
    2016-MAR-04

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -3540,6 +3540,60 @@ public class LintCode_Algorithm_Java {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/4sum/
+     * @param numbers : Give an array numbersbers of n integer
+     * @param target : you need to find four elements that's sum of target
+     * @return : Find all unique quadruplets in the array which gives the sum of
+     *           zero.
+     */
+    public ArrayList<ArrayList<Integer>> fourSum(int[] num, int target) {
+        /* your code */
+        ArrayList<ArrayList<Integer>> rst = new ArrayList<ArrayList<Integer>>();
+        Arrays.sort(num);
+
+        for (int i = 0; i < num.length - 3; i++) {
+            if (i != 0 && num[i] == num[i - 1]) {
+                continue;
+            }
+
+            for (int j = i + 1; j < num.length - 2; j++) {
+                if (j != i + 1 && num[j] == num[j - 1])
+                    continue;
+
+                int left = j + 1;
+                int right = num.length - 1;
+                while (left < right) {
+                    int sum = num[i] + num[j] + num[left] + num[right];
+                    if (sum < target) {
+                        left++;
+                    } else if (sum > target) {
+                        right--;
+                    } else {
+                        ArrayList<Integer> tmp = new ArrayList<Integer>();
+                        tmp.add(num[i]);
+                        tmp.add(num[j]);
+                        tmp.add(num[left]);
+                        tmp.add(num[right]);
+                        rst.add(tmp);
+                        left++;
+                        right--;
+                        while (left < right && num[left] == num[left - 1]) {
+                            left++;
+                        }
+                        while (left < right && num[right] == num[right + 1]) {
+                            right--;
+                        }
+                    }
+                }
+            }
+        }
+
+        return rst;
+    }
+
+
+
 
 
 
