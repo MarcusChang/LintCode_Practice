6f9bc04264a93e341abb04de7e43545716fe1bd2
Marcus Chang <marcus_chang@aliyun.com>
2016-03-04 01:20:13 +0800

    LintCode Algorithm Practice
    Chapter 7 updated http://www.lintcode.com/zh-cn/problem/4sum/
    
    Marcus_Chang
    
    2016-MAR-04

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -3542,7 +3542,7 @@ public class LintCode_Algorithm_Java {
 
     /**
      * http://www.lintcode.com/zh-cn/problem/4sum/
-     * @param numbers : Give an array numbersbers of n integer
+     * @param num : Give an array numbersbers of n integer
      * @param target : you need to find four elements that's sum of target
      * @return : Find all unique quadruplets in the array which gives the sum of
      *           zero.
