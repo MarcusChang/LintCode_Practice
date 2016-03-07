9883b9735befb0ad661cb49d739e4232b5b7f1e6
Marcus Chang <marcus_chang@aliyun.com>
2016-03-08 00:42:32 +0800

    LintCode Algorithm Practice
    Chapter 7 added http://www.lintcode.com/zh-cn/problem/majority-number-ii/
    
    Marcus_Chang
    
    2016-MAR-08

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -3674,7 +3674,7 @@ public class LintCode_Algorithm_Java {
      * @param k: As described
      * @return: The majority number
      */
-    public int majorityNumber(ArrayList<Integer> nums, int k) {
+    public int majorityNumberIII(ArrayList<Integer> nums, int k) {
         // write your code
         // count at most k keys.
         HashMap<Integer, Integer> counters = new HashMap<Integer, Integer>();
@@ -3730,6 +3730,46 @@ public class LintCode_Algorithm_Java {
             counters.remove(key);
         }
     }
+
+
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/majority-number-ii/
+     * @param nums: A list of integers
+     * @return: The majority number that occurs more than 1/3
+     */
+    public int majorityNumberII(ArrayList<Integer> nums) {
+        // write your code
+        int candidate1 = 0, candidate2 = 0;
+        int count1, count2;
+        count1 = count2 = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (candidate1 == nums.get(i)) {
+                count1 ++;
+            } else if (candidate2 == nums.get(i)) {
+                count2 ++;
+            } else if (count1 == 0) {
+                candidate1 = nums.get(i);
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = nums.get(i);
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+        count1 = count2 = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i) == candidate1) {
+                count1++;
+            } else if (nums.get(i) == candidate2) {
+                count2++;
+            }
+        }
+        return count1 > count2 ? candidate1 : candidate2;
+    }
+
 
 
 
