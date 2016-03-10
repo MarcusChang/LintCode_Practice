635e0e48e3619afcfedfc399cf9deee29f2d6c98
Marcus Chang <marcus_chang@aliyun.com>
2016-03-11 02:46:43 +0800

    LintCode Algorithm Practice
    Chapter 7 added http://www.lintcode.com/zh-cn/problem/maximum-subarray-iii/
    
    Marcus_Chang
    
    2016-MAR-11

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -3885,6 +3885,51 @@ public class LintCode_Algorithm_Java {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/maximum-subarray-iii/
+     * @param nums: A list of integers
+     * @param k: An integer denote to find k non-overlapping subarrays
+     * @return: An integer denote the sum of max k non-overlapping subarrays
+     */
+    public int maxSubArrayIII(int[] nums, int k) {
+        // write your code here
+        int len = nums.length;
+        int[][] f = new int[k+1][len];
+        for (int i = 1; i < k+1; i++) {
+            int sum = 0;
+            for (int j = 0; j < i; j++) {
+                sum += nums[j];
+            }
+            f[i][i-1] = sum;
+        }
+        for (int i = 1; i < len; i++) {
+            f[1][i] = Math.max(f[1][i-1]+nums[i], nums[i]);
+        }
+
+        for (int i = 2; i < k+1; i++) {
+            for (int n = i;  n< len; n++) {
+                int curMax = f[i][n-1] + nums[n];
+                for (int j = i-2; j < n; j++) {
+                    if ((f[i-1][j] + nums[n]) > curMax) {
+                        curMax = f[i-1][j] + nums[n];
+                    }
+                }
+                f[i][n] = curMax;
+            }
+        }
+
+        int res = Integer.MIN_VALUE;
+        for (int i = k-1; i < len; i++){
+            if (f[k][i] > res) {
+                res = f[k][i];
+            }
+        }
+        return res;
+    }
+
+
+
+
 
 
 
