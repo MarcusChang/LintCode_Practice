6ddc0cbef9c868f6f30718c84b3061968075641a
Marcus Chang <marcus_chang@aliyun.com>
2016-03-13 00:21:40 +0800

    LintCode Algorithm Practice
    Chapter 8 added http://www.lintcode.com/zh-cn/problem/implement-stack-by-two-queues/
    
    Marcus_Chang
    
    2016-MAR-13

--- src/test/java/LintCodeStack.java
+++ src/test/java/LintCodeStack.java
@@ -42,4 +42,67 @@ public class LintCodeStack {
         private List<Integer> array = new ArrayList<Integer>();
     }
 
+
+
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/implement-stack-by-two-queues/
+     * */
+    class StackByTwoQueues {
+        private Queue<Integer> queue1;
+        private Queue<Integer> queue2;
+
+        public StackByTwoQueues() {
+            queue1 = new LinkedList<Integer>();
+            queue2 = new LinkedList<Integer>();
+        }
+
+        private void moveItems() {
+            while (queue1.size() != 1) {
+                queue2.offer(queue1.poll());
+            }
+        }
+
+        private void swapQueues() {
+            Queue<Integer> temp = queue1;
+            queue1 = queue2;
+            queue2 = temp;
+        }
+
+        /**
+         * push a new item into the stack
+         */
+        public void StackByTwoQueuesPush(int value) {
+            queue1.offer(value);
+        }
+
+        /**
+         * return the top of the stack
+         */
+        public int StackByTwoQueuesTop() {
+            moveItems();
+            int item = queue1.poll();
+            swapQueues();
+            queue1.offer(item);
+            return item;
+        }
+
+        /**
+         * pop the top of the stack and return it
+         */
+        public void StackByTwoQueuesPop() {
+            moveItems();
+            queue1.poll();
+            swapQueues();
+        }
+
+        /**
+         * check the stack is empty or not.
+         */
+        public boolean StackByTwoQueuesIsEmpty() {
+            return queue1.isEmpty();
+        }
+
+    }
+
 }
