022fecffe7d78943b8a846b2487b08ced39786f6
Marcus Chang <marcus_chang@aliyun.com>
2016-03-16 01:15:17 +0800

    LintCode Algorithm Practice
    Chapter 8 added http://www.lintcode.com/zh-cn/problem/heapify/
    
    Marcus_Chang
    
    2016-MAR-16

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -4126,6 +4126,43 @@ public class LintCode_Algorithm_Java {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/heapify/
+     * @param A: Given an integer array
+     * @return: void
+     */
+
+    private void siftdown(int[] A, int k) {
+        while (k < A.length) {
+            int smallest = k;
+            if (k * 2 + 1 < A.length && A[k * 2 + 1] < A[smallest]) {
+                smallest = k * 2 + 1;
+            }
+            if (k * 2 + 2 < A.length && A[k * 2 + 2] < A[smallest]) {
+                smallest = k * 2 + 2;
+            }
+            if (smallest == k) {
+                break;
+            }
+            int temp = A[smallest];
+            A[smallest] = A[k];
+            A[k] = temp;
+
+            k = smallest;
+        }
+    }
+
+    public void heapify(int[] A) {
+        // write your code here
+        for (int i = A.length / 2; i >= 0; i--) {
+            siftdown(A, i);
+        } // for
+    }
+
+
+
+
+
 
     /*
     * JiuZhang Chapter 9 begins !
