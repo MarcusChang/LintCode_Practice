a2d404221966ab568dad3014eee242778fbda951
Marcus Chang <marcus_chang@aliyun.com>
2016-03-19 01:58:02 +0800

    LintCode Algorithm Practice
    Chapter 5 added http://www.lintcode.com/zh-cn/problem/decode-ways/
    
    Marcus_Chang
    
    2016-MAR-19

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -2141,6 +2141,39 @@ public class LintCode_Algorithm_Java {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/decode-ways/
+     * @param s a string,  encoded message
+     * @return an integer, the number of ways decoding
+     */
+    public int numDecodings(String s) {
+        // Write your code here
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int[] nums = new int[s.length() + 1];
+        nums[0] = 1;
+        nums[1] = s.charAt(0) != '0' ? 1 : 0;
+        for (int i = 2; i <= s.length(); i++) {
+            if (s.charAt(i - 1) != '0') {
+                nums[i] = nums[i - 1];
+            }
+
+            int twoDigits = (s.charAt(i - 2) - '0') * 10 + s.charAt(i - 1) - '0';
+            if (twoDigits >= 10 && twoDigits <= 26) {
+                nums[i] += nums[i - 2];
+            }
+        }
+        return nums[s.length()];
+    }
+
+
+
+
+
+
+
+
 
      /*
     * JiuZhang Chapter 6 begins !
