5de32743bc314d44c7b25dfe736c33bbb59f414e
Marcus Chang <marcus_chang@aliyun.com>
2016-03-20 00:37:15 +0800

    LintCode Algorithm Practice
    Chapter 5 added http://www.lintcode.com/zh-cn/problem/minimum-adjustment-cost/
    
    Marcus_Chang
    
    2016-MAR-20

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -2171,6 +2171,38 @@ public class LintCode_Algorithm_Java {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/minimum-adjustment-cost/
+     * @param A: An integer array.
+     * @param target: An integer.
+     */
+    public int MinAdjustmentCost(ArrayList<Integer> A, int target) {
+        // write your code here
+        int n = A.size();
+        int[][] f = new int[n + 1][101];
+        for (int i = 0; i <= n ; ++i)
+            for (int j = 0; j <=100; ++j)
+                f[i][j] = 1000000;
+        for (int i = 0; i <=100; ++i)
+            f[0][i] = 0;
+        for (int i = 1; i <=n; ++i)
+            for (int  j = 0; j <= 100;++j)
+                if (f[i-1][j] != 1000000)
+                    for (int k = 0; k <= 100; ++k)
+                        if (Math.abs(j-k) <= target)
+                            if (f[i][k] > f[i-1][j] + Math.abs(A.get(i-1)-k))
+                                f[i][k] = f[i-1][j] + Math.abs(A.get(i-1)-k);
+        int ans =1000000;
+        for (int i = 0; i <= 100; ++i)
+            if (f[n][i] < ans)
+                ans = f[n][i];
+        return ans;
+    }
+
+
+
+
+
 
 
 
