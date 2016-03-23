44575fead617d515670fb93477196c704b062567
Marcus Chang <marcus_chang@aliyun.com>
2016-03-24 01:37:15 +0800

    LintCode Algorithm Practice
    Chapter 6 added http://www.lintcode.com/zh-cn/problem/add-two-numbers/
    
    Marcus_Chang
    
    2016-MAR-24

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -2959,6 +2959,57 @@ public class LintCode_Algorithm_Java {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/add-two-numbers/
+     * @param l1: the first list
+     * @param l2: the second list
+     * @return: the sum list of l1 and l2
+     */
+    public ListNode addLists(ListNode l1, ListNode l2) {
+        // write your code here
+        if(l1 == null && l2 == null) {
+            return null;
+        }
+
+        ListNode head = new ListNode(0);
+        ListNode point = head;
+        int carry = 0;
+        while(l1 != null && l2!=null){
+            int sum = carry + l1.val + l2.val;
+            point.next = new ListNode(sum % 10);
+            carry = sum / 10;
+            l1 = l1.next;
+            l2 = l2.next;
+            point = point.next;
+        }
+
+        while(l1 != null) {
+            int sum =  carry + l1.val;
+            point.next = new ListNode(sum % 10);
+            carry = sum /10;
+            l1 = l1.next;
+            point = point.next;
+        }
+
+        while(l2 != null) {
+            int sum =  carry + l2.val;
+            point.next = new ListNode(sum % 10);
+            carry = sum /10;
+            l2 = l2.next;
+            point = point.next;
+        }
+
+        if (carry != 0) {
+            point.next = new ListNode(carry);
+        }
+        return head.next;
+    }
+
+
+
+
+
+
 
      /*
     * JiuZhang Chapter 7 begins !
