070199564ab949d1df320d30e4597d0e12c7a4a7
Marcus Chang <marcus_chang@aliyun.com>
2016-03-26 01:30:11 +0800

    LintCode Algorithm Practice
    Chapter 8 added http://www.lintcode.com/zh-cn/problem/max-tree/
    
    Marcus_Chang
    
    2016-MAR-26

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -4384,6 +4384,37 @@ public class LintCode_Algorithm_Java {
 
 
 
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/max-tree/
+     * @param A: Given an integer array with no duplicates.
+     * @return: The root of max tree.
+     */
+    public TreeNode maxTree(int[] A) {
+        // write your code here
+        int len = A.length;
+        TreeNode[] stk = new TreeNode[len];
+        for (int i = 0; i < len; ++i)
+            stk[i] = new TreeNode(0);
+        int cnt = 0;
+        for (int i = 0; i < len; ++i) {
+            TreeNode tmp = new TreeNode(A[i]);
+            while (cnt > 0 && A[i] > stk[cnt-1].val) {
+                tmp.left = stk[cnt-1];
+                cnt --;
+            }
+            if (cnt > 0)
+                stk[cnt - 1].right = tmp;
+            stk[cnt++] = tmp;
+        }
+        return stk[0];
+    }
+
+
+
+
+
+
     /*
     * JiuZhang Chapter 9 begins !
     * */
