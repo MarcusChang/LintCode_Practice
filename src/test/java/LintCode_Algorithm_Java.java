fcc72dbb4255332540c10549b333a6a05882ab78
Marcus Chang <marcus_chang@aliyun.com>
2016-03-29 00:41:50 +0800

    LintCode Algorithm Practice
    Chapter 9 added http://www.lintcode.com/zh-cn/problem/k-sum-ii/
    
    Marcus_Chang
    
    2016-MAR-29

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -4824,6 +4824,37 @@ public class LintCode_Algorithm_Java {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/k-sum-ii/
+     * @param A: an integer array.
+     * @param k: a positive integer (k <= length(A))
+     * @param target: a integer
+     * @return an integer
+     */
+    ArrayList<ArrayList<Integer> > ans;
+    public void dfs(int A[], int K, int target, int index, ArrayList<Integer> tans)
+    {
+
+        if(K == 0 && target == 0) {
+            ans.add(new ArrayList<Integer>(tans));
+            return ;
+        }
+        if(K < 0 || target < 0 || index < 0)
+            return ;
+        dfs(A, K, target, index - 1, tans);
+        tans.add(A[index]);
+        dfs(A, K  - 1, target - A[index], index - 1, tans);
+        tans.remove(tans.size() - 1);
+
+    }
+    public ArrayList<ArrayList<Integer>> kSumII(int A[], int K, int target) {
+        ans = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> tans = new ArrayList<Integer>();
+        dfs(A, K, target, A.length - 1, tans);
+        return ans;
+    }
+
+
 
 
 
