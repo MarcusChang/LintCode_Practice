c7aa57c45102b1807a3a381d931e6e1415f42b9d
Marcus Chang <marcus_chang@aliyun.com>
2016-03-31 00:15:54 +0800

    LintCode Algorithm Practice
    Chapter 9 added http://www.lintcode.com/zh-cn/problem/find-the-connected-component-in-the-undirected-graph/
    
    Marcus_Chang
    
    2016-MAR-31

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -4930,4 +4930,57 @@ public class LintCode_Algorithm_Java {
     }
 
 
+
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/find-the-connected-component-in-the-undirected-graph/
+     * @param nodes a array of Undirected graph node
+     * @return a connected set of a Undirected graph
+     */
+    public List<List<Integer>> connectedSet(ArrayList<UndirectedGraphNode> nodes) {
+        // Write your code here
+        int m = nodes.size();
+        Map<UndirectedGraphNode, Boolean> visited = new HashMap<UndirectedGraphNode, Boolean>();
+
+        for (UndirectedGraphNode node : nodes){
+            visited.put(node, false);
+        }
+
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+        for (UndirectedGraphNode node : nodes){
+            if (visited.get(node) == false){
+                bfs(node, visited, result);
+            }
+        }
+
+        return result;
+    }
+
+
+    public void bfs(UndirectedGraphNode node, Map<UndirectedGraphNode, Boolean> visited, List<List<Integer>> result){
+
+        List<Integer>row = new ArrayList<Integer>();
+
+        Queue<UndirectedGraphNode> queue = new LinkedList<UndirectedGraphNode>();
+        visited.put(node, true);
+        queue.offer(node);
+
+        while (!queue.isEmpty()){
+            UndirectedGraphNode u = queue.poll();
+            row.add(u.label);
+
+            for (UndirectedGraphNode v : u.neighbors){
+                if (visited.get(v) == false){
+                    visited.put(v, true);
+                    queue.offer(v);
+                }
+            }
+        }
+
+        Collections.sort(row);
+        result.add(row);
+    }
+
+
 }
