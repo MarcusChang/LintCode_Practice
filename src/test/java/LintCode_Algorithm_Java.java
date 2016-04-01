148b219f1b639963ce09bc0bd2a1e3636bac412a
Marcus Chang <marcus_chang@aliyun.com>
2016-04-02 01:59:46 +0800

    LintCode Algorithm Practice
    Chapter 3 added http://www.lintcode.com/zh-cn/problem/lowest-common-ancestor-ii/
    
    Marcus_Chang
    
    2016-APR-02

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -1610,6 +1610,42 @@ public class LintCode_Algorithm_Java {
 
 
 
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/lowest-common-ancestor-ii/
+     * @param root: The root of the tree
+     * @param A, B: Two node in the tree
+     * @return: The lowest common ancestor of A and B
+     */
+    public ParentTreeNode lowestCommonAncestorII(ParentTreeNode root,
+                                                 ParentTreeNode A,
+                                                 ParentTreeNode B) {
+        // Write your code here
+        if (root == null) {
+            return null;
+        }
+        if (root == A || root == B) {
+            return root;
+        }
+
+        ParentTreeNode left = lowestCommonAncestorII(root.left, A, B);
+        ParentTreeNode right = lowestCommonAncestorII(root.right, A, B);
+        if (left != null && right != null) {
+            return root;
+        } else if (left != null) {
+            return left;
+        } else if (right != null) {
+            return right;
+        } else {
+            return null;
+        }
+    }
+
+
+
+
+
+
      /*
     * JiuZhang Chapter 4 begins !
     * */
---
+++ src/test/java/ParentTreeNode.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Marcus_Chang on 2016/4/2.
+ */
+public class ParentTreeNode {
+    public ParentTreeNode parent, left, right;
+}
