6a4512a7ce332877151f994a2fc5b49b8dcf31f5
Marcus Chang <marcus_chang@aliyun.com>
2016-04-04 01:54:28 +0800

    LintCode Algorithm Practice
    Chapter 3 added http://www.lintcode.com/zh-cn/problem/insert-node-in-a-binary-search-tree/
    
    Marcus_Chang
    
    2016-APR-04

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -1667,6 +1667,31 @@ public class LintCode_Algorithm_Java {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/insert-node-in-a-binary-search-tree/
+     * @param root: The root of the binary search tree.
+     * @param node: insert this node into the binary search tree
+     * @return: The root of the new binary search tree.
+     */
+    public TreeNode insertNode(TreeNode root, TreeNode node) {
+        // write your code here
+        if (root == null) {
+            return node;
+        }
+        if (root.val > node.val) {
+            root.left = insertNode(root.left, node);
+        } else {
+            root.right = insertNode(root.right, node);
+        }
+        return root;
+    }
+
+
+
+
+
+
+
      /*
     * JiuZhang Chapter 4 begins !
     * */
