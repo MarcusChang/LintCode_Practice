06138a28468401c75d6b4129cf457b9ce82cadef
Marcus Chang <marcus_chang@aliyun.com>
2016-04-05 00:10:08 +0800

    LintCode Algorithm Practice
    Chapter 6 added http://www.jiuzhang.com/solutions/convert-binary-search-tree-to-doubly-linked-list/
    
    Marcus_Chang
    
    2016-APR-05

---
+++ src/test/java/DoublyListNode.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Marcus_Chang on 2016/4/5.
+ */
+public class DoublyListNode {
+    int val;
+    DoublyListNode next, prev;
+    DoublyListNode(int val) {
+        this.val = val;
+        this.next = this.prev = null;
+    }
+}
