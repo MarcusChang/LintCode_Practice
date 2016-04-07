d8940dd1f69d1cf284d169d16b60253396e6f0f5
Marcus Chang <marcus_chang@aliyun.com>
2016-04-08 00:49:21 +0800

    LintCode Algorithm Practice
    Chapter 2 updated http://www.lintcode.com/zh-cn/problem/total-occurrence-of-target/
    
    Marcus_Chang
    
    2016-APR-08

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -730,6 +730,53 @@ public class LintCode_Algorithm_Java {
 
 
 
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/total-occurrence-of-target/
+     * @param A an integer array sorted in ascending order
+     * @param target an integer
+     * @return an integer
+     */
+    public int totalOccurrence(int[] A, int target) {
+        // Write your code here
+        int n = A.length;
+        if (n == 0)
+            return 0;
+        if (A[n-1] < target || A[0] > target)
+            return 0;
+
+        int l = 0, r = n - 1;
+        int start = 0;
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            if (A[mid] >= target) {
+                start = mid;
+                r = mid - 1;
+            } else
+                l = mid + 1;
+        }
+        if (A[start] != target)
+            return 0;
+
+        int end = n-1;
+        l = 0; r = n-1;
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            if (A[mid] <= target) {
+                end = mid;
+                l = mid + 1;
+            } else
+                r = mid - 1;
+        }
+        return end - start + 1;
+    }
+
+
+
+
+
+
+
     /*
     * JiuZhang Chapter 3 begins !
     * */
