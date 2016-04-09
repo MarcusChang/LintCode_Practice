8e6917e94c25a58b15239752127f4d4e213abb37
Marcus Chang <marcus_chang@aliyun.com>
2016-04-10 00:36:17 +0800

    LintCode Algorithm Practice
    Chapter 2 updated http://www.lintcode.com/zh-cn/problem/last-position-of-target/
    
    Marcus_Chang
    
    2016-APR-10

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -829,6 +829,43 @@ public class LintCode_Algorithm_Java {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/last-position-of-target/
+     * @param A an integer array sorted in ascending order
+     * @param target an integer
+     * @return an integer
+     */
+    public int lastPosition(int[] A, int target) {
+        // Write your code here
+        int lenA = A.length;
+
+        if(lenA == 0 || A == null || A[0] > target || A[lenA - 1] < target){
+            return -1;
+        }
+        int start = 0, end = lenA - 1;
+        while(start + 1< end){
+            int mid = start + (end - start) / 2;
+            if(A[mid] == target){
+                start = mid ;
+            }else if(A[mid] < target){
+                start = mid ;
+            }else{
+                end = mid;
+            }
+        }
+        if(A[end] == target){
+            return end;
+        }else if(A[start] == target){
+            return start;
+        }else{
+            return -1;
+        }
+    }
+
+
+
+
+
     /*
     * JiuZhang Chapter 3 begins !
     * */
