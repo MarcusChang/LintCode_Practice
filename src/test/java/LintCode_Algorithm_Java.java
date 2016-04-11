71c7373ac99327cb10b3d6b022b78220e412078a
Marcus Chang <marcus_chang@aliyun.com>
2016-04-12 03:11:05 +0800

    LintCode Algorithm Practice
    Chapter 2 updated http://www.lintcode.com/zh-cn/problem/k-closest-numbers-in-sorted-array/
    
    Marcus_Chang
    
    2016-APR-12

--- src/test/java/LintCode_Algorithm_Java.java
+++ src/test/java/LintCode_Algorithm_Java.java
@@ -900,6 +900,71 @@ public class LintCode_Algorithm_Java {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/k-closest-numbers-in-sorted-array/
+     * @param A an integer array
+     * @param target an integer
+     * @param k a non-negative integer
+     * @return an integer array
+     */
+    public int[] kClosestNumbers(int[] A, int target, int k) {
+        // Write your code here
+        int[] result = new int[k];
+
+        if (A == null || A.length == 0) {
+            return A;
+        }
+        if (k > A.length) {
+            return A;
+        }
+
+        int index = firstIndexOfkClosestNumbers(A, target);
+
+        int start = index - 1;
+        int end = index;
+        for (int i = 0; i < k; i++) {
+            if (start < 0) {
+                result[i] = A[end++];
+            } else if (end >= A.length) {
+                result[i] = A[start--];
+            } else {
+                if (target - A[start] <= A[end] - target) {
+                    result[i] = A[start--];
+                } else {
+                    result[i] = A[end++];
+                }
+            }
+        }
+        return result;
+    }
+
+    private int firstIndexOfkClosestNumbers(int[] A, int target) {
+        int start = 0, end = A.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (A[mid] < target) {
+                start = mid;
+            } else if (A[mid] > target) {
+                end = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (A[start] >= target) {
+            return start;
+        }
+        if (A[end] >= target) {
+            return end;
+        }
+        return A.length;
+    }
+
+
+
+
+
+
 
     /*
     * JiuZhang Chapter 3 begins !
