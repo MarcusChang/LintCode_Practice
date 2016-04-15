5d3b5f3d5d6e03a690c738b5e6a5fccbc736efe3
Marcus Chang <marcus_chang@aliyun.com>
2016-04-16 00:40:48 +0800

    LintCode Algorithm Practice FLAG
    Chapter 1 updated http://www.jiuzhang.com/solutions/two-strings-are-anagrams/
    
    Marcus_Chang
    
    2016-APR-16

---
+++ src/test/java/LintCode_FLAG_Algorithm.java
@@ -0,0 +1,40 @@
+import org.junit.Test;
+import java.util.*;
+
+/**
+ * Created by Marcus_Chang on 2016/4/16.
+ */
+public class LintCode_FLAG_Algorithm {
+
+
+    /*
+    * FLAG Chapter 1 begins !
+    * */
+
+    /**
+     * http://www.jiuzhang.com/solutions/two-strings-are-anagrams/
+     * @param s: The first string
+     * @param t: The second string
+     * @return true or false
+     */
+    public boolean anagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            count[(int) s.charAt(i)]++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            count[(int) t.charAt(i)]--;
+            if (count[(int) t.charAt(i)] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+}
