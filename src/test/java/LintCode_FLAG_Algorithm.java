0ab8d728ba1d880177fe45d890441d75ec167935
Marcus Chang <marcus_chang@aliyun.com>
2016-04-19 00:15:23 +0800

    LintCode Algorithm Practice FLAG
    Chapter 1 updated http://www.lintcode.com/zh-cn/problem/longest-common-prefix/
    
    Marcus_Chang
    
    2016-APR-19

--- src/test/java/LintCode_FLAG_Algorithm.java
+++ src/test/java/LintCode_FLAG_Algorithm.java
@@ -111,4 +111,40 @@ public class LintCode_FLAG_Algorithm {
 
 
 
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/longest-common-prefix/
+     * @param strs: A list of strings
+     * @return: The longest common prefix
+     */
+    public String longestCommonPrefix(String[] strs) {
+        // write your code here
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        for(int i = 1; i < strs.length; i++) {
+            int j = 0;
+            while( j < strs[i].length() && j < prefix.length() && strs[i].charAt(j) == prefix.charAt(j)) {
+                j++;
+            }
+            if( j == 0) {
+                return "";
+            }
+            prefix = prefix.substring(0, j);
+        }
+        return prefix;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
