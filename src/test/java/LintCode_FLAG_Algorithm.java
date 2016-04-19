569b1b85f5881646be670c231872b7a01917fd3e
Marcus Chang <marcus_chang@aliyun.com>
2016-04-20 00:08:38 +0800

    LintCode Algorithm Practice FLAG
    Chapter 1 updated http://www.lintcode.com/zh-cn/problem/string-to-integer-ii/
    
    Marcus_Chang
    
    2016-APR-20

--- src/test/java/LintCode_FLAG_Algorithm.java
+++ src/test/java/LintCode_FLAG_Algorithm.java
@@ -139,7 +139,50 @@ public class LintCode_FLAG_Algorithm {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/string-to-integer-ii/
+     * @param str: A string
+     * @return An integer
+     */
+    public int atoi(String str) {
+        // write your code here
+        if (str == null || str.length() < 1)
+            return 0;
 
+        // trim white spaces
+        str = str.trim();
+
+        char flag = '+';
+
+        // check negative or positive
+        int i = 0;
+        if (str.charAt(0) == '-') {
+            flag = '-';
+            i++;
+        } else if (str.charAt(0) == '+') {
+            i++;
+        }
+        // use double to store result
+        double result = 0;
+
+        // calculate value
+        while (str.length() > i && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+            result = result * 10 + (str.charAt(i) - '0');
+            i++;
+        }
+
+        if (flag == '-')
+            result = -result;
+
+        // handle max and min
+        if (result > Integer.MAX_VALUE)
+            return Integer.MAX_VALUE;
+
+        if (result < Integer.MIN_VALUE)
+            return Integer.MIN_VALUE;
+
+        return (int) result;
+    }
 
 
 
