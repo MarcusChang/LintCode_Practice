be3db715061bcc41b20dd795ee062c5f874f8402
Marcus Chang <marcus_chang@aliyun.com>
2016-04-21 02:01:55 +0800

    LintCode Algorithm Practice FLAG
    Chapter 2 updated http://www.lintcode.com/zh-cn/problem/remove-element/
    
    Marcus_Chang
    
    2016-APR-21

--- src/test/java/LintCode_FLAG_Algorithm.java
+++ src/test/java/LintCode_FLAG_Algorithm.java
@@ -187,6 +187,32 @@ public class LintCode_FLAG_Algorithm {
 
 
 
+    /*
+    * FLAG Chapter 2 begins !
+    * */
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/remove-element/
+     *@param A: A list of integers
+     *@param elem: An integer
+     *@return: The new length after remove
+     */
+    public int removeElement(int[] A, int elem) {
+        // write your code here
+        int i = 0;
+        int pointer = A.length - 1;
+        while(i <= pointer){
+            if(A[i] == elem){
+                A[i] = A[pointer];
+                pointer--;
+            } else {
+                i++;
+            }
+        }
+        return pointer + 1;
+    }
+
+
 
 
 
