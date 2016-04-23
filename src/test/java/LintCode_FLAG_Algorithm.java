c923874cc72c69dbf43168743c1b9c117fa37e00
Marcus Chang <marcus_chang@aliyun.com>
2016-04-24 00:35:29 +0800

    LintCode Algorithm Practice FLAG
    Chapter 2 updated http://www.lintcode.com/zh-cn/problem/first-missing-positive/
    
    Marcus_Chang
    
    2016-APR-24

--- src/test/java/LintCode_FLAG_Algorithm.java
+++ src/test/java/LintCode_FLAG_Algorithm.java
@@ -285,5 +285,39 @@ public class LintCode_FLAG_Algorithm {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/first-missing-positive/
+     * @param A: an array of integers
+     * @return: an integer
+     */
+    public int firstMissingPositive(int[] A) {
+        // write your code here
+        if (A == null) {
+            return 1;
+        }
+
+        for (int i = 0; i < A.length; i++) {
+            while (A[i] > 0 && A[i] <= A.length && A[i] != (i+1)) {
+                int tmp = A[A[i]-1];
+                if (tmp == A[i]) {
+                    break;
+                }
+                A[A[i]-1] = A[i];
+                A[i] = tmp;
+            }
+        }
+
+        for (int i = 0; i < A.length; i ++) {
+            if (A[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return A.length + 1;
+    }
+
+
+
+
 
 }
