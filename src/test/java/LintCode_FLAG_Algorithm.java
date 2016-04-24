132588ffc3929534d46f619dd1d2088e558fb91b
Marcus Chang <marcus_chang@aliyun.com>
2016-04-25 00:17:00 +0800

    LintCode Algorithm Practice FLAG
    Chapter 2 updated http://www.lintcode.com/zh-cn/problem/remove-duplicates-from-sorted-array-ii/
    
    Marcus_Chang
    
    2016-APR-25

--- src/test/java/LintCode_FLAG_Algorithm.java
+++ src/test/java/LintCode_FLAG_Algorithm.java
@@ -319,5 +319,33 @@ public class LintCode_FLAG_Algorithm {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/remove-duplicates-from-sorted-array-ii/
+     * @param nums: a array of integers
+     * @return : return an integer
+     */
+    public int removeDuplicates2(int[] nums) {
+        // write your code here
+        if(nums == null)
+            return 0;
+        int cur = 0;
+        int i ,j;
+        for(i = 0; i < nums.length;){
+            int now = nums[i];
+            for( j = i; j < nums.length; j++){
+                if(nums[j] != now)
+                    break;
+                if(j-i < 2)
+                    nums[cur++] = now;
+            }
+            i = j;
+        }
+        return cur;
+    }
+
+
+
+
+
 
 }
