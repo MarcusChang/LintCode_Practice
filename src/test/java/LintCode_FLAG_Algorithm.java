2c3e019d93ab506d5d35c142970f6108c7425d4e
Marcus Chang <marcus_chang@aliyun.com>
2016-04-28 01:34:32 +0800

    LintCode Algorithm Practice FLAG
    Chapter 4 updated http://www.lintcode.com/zh-cn/problem/update-bits/
    
    Marcus_Chang
    
    2016-APR-28

--- src/test/java/LintCode_FLAG_Algorithm.java
+++ src/test/java/LintCode_FLAG_Algorithm.java
@@ -374,6 +374,31 @@ public class LintCode_FLAG_Algorithm {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/update-bits/
+     *@param n, m: Two integer
+     *@param i, j: Two bit positions
+     *return: An integer
+     */
+    public int updateBits(int n, int m, int i, int j) {
+        // write your code here
+        int max = ~0; /* All 1’s */
+        // 1’s through position j, then 0’s
+        if (j == 31)
+            j = max;
+        else
+            j = (1 << (j + 1)) - 1;
+        int left = max - j;
+        // 1’s after position i
+        int right = ((1 << i) - 1);
+        // 1’s, with 0s between i and j
+        int mask = left | right;
+        // Clear i through j, then put m in there
+        return ((n & mask) | (m << i));
+    }
+
+
+
 
 
 }
