66d2a76eb40fecd726f9a104087df687951d1a8d
Marcus Chang <marcus_chang@aliyun.com>
2016-04-30 00:27:12 +0800

    LintCode Algorithm Practice FLAG
    Chapter 4 updated http://www.lintcode.com/zh-cn/problem/fast-power/
    
    Marcus_Chang
    
    2016-APR-30

--- src/test/java/LintCode_FLAG_Algorithm.java
+++ src/test/java/LintCode_FLAG_Algorithm.java
@@ -428,4 +428,41 @@ public class LintCode_FLAG_Algorithm {
     }
 
 
+
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/fast-power/
+     * @param a, b, n: 32bit integers
+     * @return: An integer
+     */
+    public int fastPower(int a, int b, int n) {
+        // write your code here
+        if (n == 1) {
+            return a % b;
+        }
+        if (n == 0) {
+            return 1 % b;
+        }
+
+        long product = fastPower(a, b, n / 2);
+        product = (product * product) % b;
+        if (n % 2 == 1) {
+            product = (product * a) % b;
+        }
+        return (int) product;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
