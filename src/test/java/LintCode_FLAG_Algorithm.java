581c757f0df58cf1a0671a1339c7c5c65683341f
Marcus Chang <marcus_chang@aliyun.com>
2016-05-02 01:05:22 +0800

    LintCode Algorithm Practice FLAG
    Chapter 5 http://www.lintcode.com/zh-cn/problem/gas-station/
    
    Marcus_Chang
    
    2016-MAY-2

--- src/test/java/LintCode_FLAG_Algorithm.java
+++ src/test/java/LintCode_FLAG_Algorithm.java
@@ -516,5 +516,40 @@ public class LintCode_FLAG_Algorithm {
 
 
 
+    /*
+    * FLAG Chapter 5 begins !
+    * */
+
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/gas-station/
+     * @param gas: an array of integers
+     * @param cost: an array of integers
+     * @return: an integer
+     */
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        // write your code here
+        if (gas == null || cost == null || gas.length == 0 || cost.length == 0) {
+            return -1;
+        }
+
+        int sum = 0;
+        int total = 0;
+        int index = -1;
+
+        for(int i = 0; i<gas.length; i++) {
+            sum += gas[i] - cost[i];
+            total += gas[i] - cost[i];
+            if(sum < 0) {
+                index = i;
+                sum = 0;
+            }
+        }
+        return total < 0 ? -1 : index + 1;
+        // index should be updated here for cases ([5], [4]);
+        // total < 0 is for case [2], [2]
+    }
+
+
 
 }
