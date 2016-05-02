c11c70edef51f09bdca03843a6b978785eba5d8c
Marcus Chang <marcus_chang@aliyun.com>
2016-05-03 00:05:50 +0800

    LintCode Algorithm Practice FLAG
    Chapter 5 http://www.jiuzhang.com/solutions/delete-digits/
    
    Marcus_Chang
    
    2016-MAY-3

--- src/test/java/LintCode_FLAG_Algorithm.java
+++ src/test/java/LintCode_FLAG_Algorithm.java
@@ -552,4 +552,32 @@ public class LintCode_FLAG_Algorithm {
 
 
 
+
+    /**
+     * http://www.jiuzhang.com/solutions/delete-digits/
+     *@param A: A positive integer which has N digits, A is a string.
+     *@param k: Remove k digits.
+     *@return: A string
+     */
+    public String DeleteDigits(String A, int k) {
+        // write your code here
+        StringBuffer sb = new StringBuffer(A);
+        int i, j;
+        for (i = 0; i < k; i++) {
+            for (j = 0; j < sb.length() - 1
+                    && sb.charAt(j) <= sb.charAt(j + 1); j++) {
+            }
+            sb.delete(j, j + 1);
+        }
+        while (sb.length() > 1 && sb.charAt(0)=='0')
+            sb.delete(0,1);
+        return sb.toString();
+    }
+
+
+
+
+
+
+
 }
