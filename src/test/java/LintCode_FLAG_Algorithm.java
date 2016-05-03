2a7f8569799c5787fba39537c4da00118d9accc6
Marcus Chang <marcus_chang@aliyun.com>
2016-05-04 01:14:47 +0800

    LintCode Algorithm Practice FLAG
    Chapter 5 http://www.lintcode.com/zh-cn/problem/next-permutation/
    
    Marcus_Chang
    
    2016-MAY-4

--- src/test/java/LintCode_FLAG_Algorithm.java
+++ src/test/java/LintCode_FLAG_Algorithm.java
@@ -578,6 +578,52 @@ public class LintCode_FLAG_Algorithm {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/next-permutation/
+     * @param nums: an array of integers
+     * @return: return nothing (void), do not return anything, modify nums in-place instead
+     */
+    public int[] nextPermutation(int[] nums) {
+        // write your code here
+        if (nums == null) {
+            return nums;
+        }
+
+        int len = nums.length;
+        for (int i = len - 2; i >= 0; i--) {
+            if (nums[i + 1] > nums[i]) {
+                int j;
+                for (j = len - 1; j > i - 1; j--) {
+                    if (nums[j] > nums[i]) {
+                        break;
+                    }
+                }
+
+                swap(nums, i, j);
+                reverse(nums, i + 1, len-1);
+                return nums;
+            }
+        }
+
+        reverse(nums, 0, len-1);
+        return nums;
+    }
+
+    void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+    void reverse(int[] nums, int beg, int end) {
+        for (int i = beg, j = end; i < j; i ++, j --) {
+            swap(nums, i, j);
+        }
+    }
+
+
+
+
 
 
 }
