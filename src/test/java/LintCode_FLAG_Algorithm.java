f0b0d72390418d9fdb2f8f70447418a415d56300
Marcus Chang <marcus_chang@aliyun.com>
2016-05-05 00:41:03 +0800

    LintCode Algorithm Practice FLAG
    Chapter 8 http://www.lintcode.com/zh-cn/problem/combinations/
    
    Marcus_Chang
    
    2016-MAY-5

--- src/test/java/LintCode_FLAG_Algorithm.java
+++ src/test/java/LintCode_FLAG_Algorithm.java
@@ -626,4 +626,46 @@ public class LintCode_FLAG_Algorithm {
 
 
 
+
+    /*
+    * FLAG Chapter 8 begins !
+    * */
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/combinations/
+     * @param n: Given the range of numbers
+     * @param k: Given the numbers of combinations
+     * @return: All the combinations of k numbers out of 1..n
+     */
+    public ArrayList<ArrayList<Integer>> combine(int n, int k) {
+        ArrayList<ArrayList<Integer>> rst = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> solution = new ArrayList<Integer>();
+
+        combineHelper(rst, solution, n, k, 1);
+        return rst;
+    }
+
+    private void combineHelper(
+            ArrayList<ArrayList<Integer>> rst,
+            ArrayList<Integer> solution,
+            int n,
+            int k,
+            int start) {
+
+        if (solution.size() == k){
+            rst.add(new ArrayList(solution));
+            return;
+        }
+
+        for(int i = start; i<= n; i++){
+            solution.add(i);
+
+            // the new start should be after the next number after i
+            combineHelper(rst, solution, n, k, i+1);
+            solution.remove(solution.size() - 1);
+        }
+    }
+
+
+
 }
