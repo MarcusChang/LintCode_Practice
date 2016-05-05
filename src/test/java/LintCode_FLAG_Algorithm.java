ad2ba7e35ec04be85508faa27f62d458971d4a84
Marcus Chang <marcus_chang@aliyun.com>
2016-05-06 00:16:50 +0800

    LintCode Algorithm Practice FLAG
    Chapter 8 http://www.lintcode.com/zh-cn/problem/combination-sum-ii/
    
    Marcus_Chang
    
    2016-MAY-6

--- src/test/java/LintCode_FLAG_Algorithm.java
+++ src/test/java/LintCode_FLAG_Algorithm.java
@@ -625,8 +625,6 @@ public class LintCode_FLAG_Algorithm {
 
 
 
-
-
     /*
     * FLAG Chapter 8 begins !
     * */
@@ -665,6 +663,55 @@ public class LintCode_FLAG_Algorithm {
             solution.remove(solution.size() - 1);
         }
     }
+
+
+
+
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/combination-sum-ii/
+     * @param candidates: Given the candidate numbers
+     * @param target: Given the target number
+     * @return: All the combinations that sum to target
+     */
+    public ArrayList<ArrayList<Integer>> combinationSum2(int[] candidates, int target) {
+        if (candidates.length < 1) {
+            return results;
+        }
+
+        ArrayList<Integer> path = new ArrayList<Integer>();
+        java.util.Arrays.sort(candidates);
+        results = new ArrayList<ArrayList<Integer>> ();
+        combinationSumHelper(path, candidates, target, 0);
+
+        return results;
+    }
+
+    private ArrayList<ArrayList<Integer>> results;
+
+    private void combinationSumHelper(ArrayList<Integer> path, int[] candidates, int sum, int pos) {
+        if (sum == 0) {
+            results.add(new ArrayList<Integer>(path));
+        }
+
+        if (pos >= candidates.length || sum < 0) {
+            return;
+        }
+
+        int prev = -1;
+        for (int i = pos; i < candidates.length; i++) {
+            if (candidates[i] != prev) {
+                path.add(candidates[i]);
+                combinationSumHelper(path, candidates, sum - candidates[i], i + 1);
+                prev = candidates[i];
+                path.remove(path.size()-1);
+            }
+        }
+    }
+
+
+
+
 
 
 
