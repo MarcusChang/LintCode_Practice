5864e31c9f89bee1f080f3b4151ce42da94aed27
Marcus Chang <marcus_chang@aliyun.com>
2016-05-09 00:16:23 +0800

    LintCode Algorithm Practice CI
    Chapter 2 http://www.lintcode.com/zh-cn/problem/fibonacci/
    
    Marcus_Chang
    
    2016-MAY-9

---
+++ src/test/java/LintCode_CodingInterviews.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+import java.util.*;
+
+/**
+ * Created by Marcus_Chang on 2016/5/9.
+ */
+public class LintCode_CodingInterviews {
+
+
+    /*
+    * CI Chapter 2 begins !
+    * */
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/fibonacci/
+     * @param n: an integer
+     * @return an integer f(n)
+     */
+    public int fibonacci(int n) {
+        // write your code here
+        int a = 0;
+        int b = 1;
+        for (int i = 0; i < n - 1; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        return a;
+    }
+
+
+}
