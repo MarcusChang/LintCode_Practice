394504509880d1d595686757adfe86c2f806f677
Marcus Chang <marcus_chang@aliyun.com>
2016-05-12 00:34:40 +0800

    LintCode Algorithm Practice CI
    Chapter 2 http://www.jiuzhang.com/solutions/space-replacement/
    
    Marcus_Chang
    
    2016-MAY-12

--- src/test/java/LintCode_CodingInterviews.java
+++ src/test/java/LintCode_CodingInterviews.java
@@ -68,4 +68,52 @@ public class LintCode_CodingInterviews {
     }
 
 
+
+
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/space-replacement/
+     * @param string: An array of Char
+     * @param length: The true length of the string
+     * @return: The true length of new string
+     */
+    public int replaceBlank(char[] string, int length) {
+        if(0==length) return 0;
+        int num = 0;
+        for(int i=0;i<length;i++){
+            if(string[i] == ' ') num++;
+        }
+
+        int newLen = length + num*2;
+        string[newLen] = 0;
+        int j = 1;
+        for(int i=length-1;i>=0;i--){
+            if(string[i] != ' '){
+                string[newLen - j] = string[i];
+                j++;
+            }
+            else{
+                string[newLen - j] = '0';
+                j++;
+                string[newLen - j] = '2';
+                j++;
+                string[newLen - j] = '%';
+                j++;
+            }
+        }
+        return newLen;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
