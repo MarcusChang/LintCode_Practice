56cfa06afd9a68b6a60ff2a3ddc25f8c63fc84af
Marcus Chang <marcus_chang@aliyun.com>
2016-05-14 00:16:41 +0800

    LintCode Algorithm Practice CI
    Chapter 3 http://www.lintcode.com/zh-cn/problem/subtree/
    
    Marcus_Chang
    
    2016-MAY-14

--- src/test/java/LintCode_CodingInterviews.java
+++ src/test/java/LintCode_CodingInterviews.java
@@ -136,5 +136,41 @@ public class LintCode_CodingInterviews {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/subtree/
+     * @param T1, T2: The roots of binary tree.
+     * @return: True if T2 is a subtree of T1, or false.
+     */
+    public boolean isSubtree(TreeNode T1, TreeNode T2) {
+        // write your code here
+        if (T2 == null) {
+            return true;
+        }
+        if (T1 == null) {
+            return false;
+        }
+
+        if (isEqual(T1, T2)) {
+            return true;
+        }
+        if (isSubtree(T1.left, T2) || isSubtree(T1.right, T2)) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isEqual(TreeNode T1, TreeNode T2) {
+        if (T1 == null || T2 == null) {
+            return T1 == T2;
+        }
+        if (T1.val != T2.val) {
+            return false;
+        }
+        return isEqual(T1.left, T2.left) && isEqual(T1.right, T2.right);
+    }
+
+
+
+
 
 }
