f84dd955864d62e19186badb5a903b3691c04860
Marcus Chang <marcus_chang@aliyun.com>
2016-05-15 01:13:02 +0800

    LintCode Algorithm Practice CI
    Chapter 3 http://www.lintcode.com/zh-cn/problem/print-numbers-by-recursion/
    
    Marcus_Chang
    
    2016-MAY-15

--- src/test/java/LintCode_CodingInterviews.java
+++ src/test/java/LintCode_CodingInterviews.java
@@ -173,4 +173,37 @@ public class LintCode_CodingInterviews {
 
 
 
+    /*
+    * CI Chapter 3 begins !
+    * */
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/print-numbers-by-recursion/
+     * @param n: An integer.
+     * return : An array storing 1 to the largest number with n digits.
+     */
+    public List<Integer> numbersByRecursion(int n) {
+        // write your code here
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        num(n, 0, res);
+        return res;
+    }
+
+    public void num(int n, int ans,ArrayList<Integer> res){
+
+        if(n==0){
+            if(ans>0){
+                res.add(ans);
+            }
+            return;
+        }
+
+        int i;
+        for(i=0; i<=9; i++){
+            num(n-1, ans*10+i, res);
+        }
+    }
+
+
+
 }
