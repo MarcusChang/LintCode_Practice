9a5ad865b8d0c7efe143146538f64b85bc00a190
Marcus Chang <marcus_chang@aliyun.com>
2016-05-16 00:14:44 +0800

    LintCode Algorithm Practice CI
    Chapter 4 http://www.lintcode.com/zh-cn/problem/string-permutation/
    
    Marcus_Chang
    
    2016-MAY-16

--- src/test/java/LintCode_CodingInterviews.java
+++ src/test/java/LintCode_CodingInterviews.java
@@ -206,4 +206,37 @@ public class LintCode_CodingInterviews {
 
 
 
+
+    /*
+    * CI Chapter 4 begins !
+    * */
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/string-permutation/
+     * @param A a string
+     * @param B a string
+     * @return a boolean
+     */
+    public boolean stringPermutation(String A, String B) {
+        // Write your code here
+        int[] cnt = new int[1000];
+        for (int i = 0; i < A.length(); ++i)
+            cnt[(int)A.charAt(i)] += 1;
+        for (int i = 0; i < B.length(); ++i)
+            cnt[(int)B.charAt(i)] -= 1;
+        for (int i = 0; i < 1000; ++i)
+            if (cnt[i] != 0)
+                return false;
+        return true;
+    }
+
+
+
+
+
+
+
+
+
+
 }
