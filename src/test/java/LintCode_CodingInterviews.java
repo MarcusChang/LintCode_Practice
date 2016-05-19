251eac325b67d0a9f3f88b8d23872e493ddd92dc
Marcus Chang <marcus_chang@aliyun.com>
2016-05-20 00:21:19 +0800

    LintCode Algorithm Practice CI
    Chapter 5 http://www.lintcode.com/zh-cn/problem/intersection-of-two-linked-lists/
    
    Marcus_Chang
    
    2016-MAY-20

--- src/test/java/LintCode_CodingInterviews.java
+++ src/test/java/LintCode_CodingInterviews.java
@@ -387,7 +387,50 @@ public class LintCode_CodingInterviews {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/intersection-of-two-linked-lists/
+     * @param headA: the first list
+     * @param headB: the second list
+     * @return: a ListNode
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        // Write your code here
+        if (headA == null || headB == null) {
+            return null;
+        }
 
+        // get the tail of list A.
+        ListNode node = headA;
+        while (node.next != null) {
+            node = node.next;
+        }
+        node.next = headB;
+        ListNode result = listCycleII(headA);
+        node.next = null;
+        return result;
+    }
+
+    private ListNode listCycleII(ListNode head) {
+        ListNode slow = head, fast = head.next;
+
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return null;
+            }
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        slow = head;
+        fast = fast.next;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+    }
 
 
 
