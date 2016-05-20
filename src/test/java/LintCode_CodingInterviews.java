ea22ff81700986ecf3106cecaa5f0020cf00a10e
Marcus Chang <marcus_chang@aliyun.com>
2016-05-21 00:09:59 +0800

    LintCode Algorithm Practice CI
    Chapter 5 http://www.lintcode.com/zh-cn/problem/kth-largest-element/
    
    Marcus_Chang
    
    2016-MAY-21

--- src/test/java/LintCode_CodingInterviews.java
+++ src/test/java/LintCode_CodingInterviews.java
@@ -437,4 +437,69 @@ public class LintCode_CodingInterviews {
 
 
 
+    /**
+     * http://www.lintcode.com/zh-cn/problem/kth-largest-element/
+     * @param k : description of k
+     * @param nums : array of nums
+     * @return: description of return
+     */
+    public int kthLargestElement(int k, int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (k <= 0) {
+            return 0;
+        }
+        return Khelper(nums, 0, nums.length - 1, nums.length - k + 1);
+
+    }
+    public int Khelper(int[] nums, int l, int r, int k) {
+        if (l == r) {
+            return nums[l];
+        }
+        int position = partition(nums, l, r);
+        if (position + 1 == k) {
+            return nums[position];
+        } else if (position + 1 < k) {
+            return Khelper(nums, position + 1, r, k);
+        }  else {
+            return Khelper(nums, l, position - 1, k);
+        }
+    }
+    public int partition(int[] nums, int l, int r) {
+        // 初始化左右指针和pivot
+        int left = l, right = r;
+        int pivot = nums[left];
+
+        // 进行partition
+        while (left < right) {
+            while (left < right && nums[right] >= pivot) {
+                right--;
+            }
+            nums[left] = nums[right];
+            while (left < right && nums[left] <= pivot) {
+                left++;
+            }
+            nums[right] = nums[left];
+        }
+
+        // 返还pivot点到数组里面
+        nums[left] = pivot;
+        return left;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
